a66752e0d21916c919860c1d94c34c35874d896b
jalddak <jalddak98@gmail.com>
2025-04-26 16:53:37 +0900

    feat: 홈화면, 에러화면 관련 view, controller 구현 및 일부 페이지 수정 (#5)

---
+++ src/main/java/pokemon/pokedex/ErrorCheckController.java
@@ -0,0 +1,39 @@
+package pokemon.pokedex;
+
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.io.IOException;
+
+/**
+ * 에러 페이지 확인용. 확인하고 나면 @Controller 주석처리
+ */
+
+//@Controller
+public class ErrorCheckController {
+
+    @GetMapping("/error-400")
+    public String error400(HttpServletResponse response) throws IOException {
+        response.sendError(HttpServletResponse.SC_BAD_REQUEST);
+        return "error/4xx";
+    }
+
+    @GetMapping("/error-404")
+    public String error404(HttpServletResponse response) throws IOException {
+        response.sendError(HttpServletResponse.SC_NOT_FOUND);
+        return "error/4xx";
+    }
+
+    @GetMapping("/error-500")
+    public String error500(HttpServletResponse response) throws IOException {
+        response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        return "error/4xx";
+    }
+
+    @GetMapping("/error-502")
+    public String error502(HttpServletResponse response) throws IOException {
+        response.sendError(HttpServletResponse.SC_BAD_GATEWAY);
+        return "error/4xx";
+    }
+
+}
---
+++ src/main/java/pokemon/pokedex/TestData.java
@@ -0,0 +1,58 @@
+package pokemon.pokedex;
+
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import pokemon.pokedex.user.domain.AdminRequestStatus;
+import pokemon.pokedex.user.domain.Role;
+import pokemon.pokedex.user.domain.User;
+import pokemon.pokedex.user.dto.RegisterDTO;
+import pokemon.pokedex.user.repository.UserRepository;
+
+/**
+ * 메모리 기반 레포지토리로 실행 시 초기값을 넣어주기 위함.
+ * 다른 레포지토리로 실행 시, @Component 주석처리
+ */
+@Component
+@RequiredArgsConstructor
+public class TestData {
+
+    private final UserRepository userRepository;
+
+    @PostConstruct
+    public void init() {
+        RegisterDTO adminDto = new RegisterDTO();
+        adminDto.setUsername("관리자");
+        adminDto.setLoginId("admin");
+        adminDto.setEmail("admin@email.com");
+        adminDto.setPassword("test");
+        adminDto.setConfirmPassword("test");
+
+        User admin = User.createByRegisterDto(adminDto);
+        admin.setRole(Role.ADMIN);
+
+        RegisterDTO userDto1 = new RegisterDTO();
+        userDto1.setUsername("유저1");
+        userDto1.setLoginId("user1");
+        userDto1.setEmail("user1@email.com");
+        userDto1.setPassword("test");
+        userDto1.setConfirmPassword("test");
+
+        User user1 = User.createByRegisterDto(userDto1);
+
+        RegisterDTO userDto2 = new RegisterDTO();
+        userDto2.setUsername("유저2");
+        userDto2.setLoginId("user2");
+        userDto2.setEmail("user2@email.com");
+        userDto2.setPassword("test");
+        userDto2.setConfirmPassword("test");
+
+        User user2 = User.createByRegisterDto(userDto2);
+        user2.setAdminRequestStatus(AdminRequestStatus.REQUESTED);
+
+        userRepository.save(admin);
+        userRepository.save(user1);
+        userRepository.save(user2);
+    }
+
+}
