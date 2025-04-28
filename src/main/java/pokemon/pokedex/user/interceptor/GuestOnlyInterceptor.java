3bc5d2d149b102eb5b7a90c0373f3700755f750f
jalddak <jalddak98@gmail.com>
2025-04-28 20:21:11 +0900

    fix: 로그인한 사용자가 로그인/회원가입 폼에 접근할 수 있는 문제 수정 (#11)

---
+++ src/main/java/pokemon/pokedex/user/interceptor/GuestOnlyInterceptor.java
@@ -0,0 +1,21 @@
+package pokemon.pokedex.user.interceptor;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.web.servlet.HandlerInterceptor;
+import pokemon.pokedex.SessionConst;
+
+public class GuestOnlyInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        HttpSession session = request.getSession(false);
+
+        if (session != null && session.getAttribute(SessionConst.LOGIN_RESPONSE_DTO) != null) {
+            response.sendRedirect(request.getContextPath() + "/");
+            return false;
+        }
+        return true;
+    }
+}
---
+++ src/test/java/pokemon/pokedex/user/interceptor/GuestOnlyInterceptorTest.java
@@ -0,0 +1,53 @@
+package pokemon.pokedex.user.interceptor;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import pokemon.pokedex.SessionConst;
+import pokemon.pokedex.user.dto.LoginResponseDTO;
+
+import java.util.stream.Stream;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class GuestOnlyInterceptorTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    private static Stream<Arguments> provideArguments() {
+        return Stream.of(
+                Arguments.of("/login", "loginForm"),
+                Arguments.of("/register", "registerForm")
+
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideArguments")
+    @DisplayName("게스트 접근")
+    void guest(String url, String expectedFormName) throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get(url))
+                .andExpect(status().isOk())
+                .andExpect(view().name(expectedFormName));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"/login", "/register"})
+    @DisplayName("로그인 유저 접근")
+    void loginUser(String url) throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get(url)
+                        .sessionAttr(SessionConst.LOGIN_RESPONSE_DTO, new LoginResponseDTO()))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/"));
+    }
+}
